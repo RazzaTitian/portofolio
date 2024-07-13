1f2c7f8c9f1b33811460acb923bb94df53587255
RazzaTitian <zeusprototype01@gmail.com>
2024-07-13 17:02:36 +0700

    fix: changed title of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Muhammad Razza | Elden Lord",
+  title: "Razza's Portofolio",
   description: "This is a website.",
 };
 
